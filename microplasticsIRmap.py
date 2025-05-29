cf386292fb6c1e0e884e6bbacbd4461b7264d870
Michael Greenop <micgre@ceh.ac.uk>
2025-05-29 10:30:20 +0100

    Outline of the first function

--- microplasticsIRmap.py
+++ microplasticsIRmap.py
@@ -7,7 +7,7 @@ import sklearn
 import seaborn
 import scipy
 
-# Function 1: Input data format check (is the data in the format that the software requires?)
+# Function 1: Input data and format check (is the data in the format that the software requires?)
 
 # Is the first column that sample names?
 # Is the first column name "studyid"
@@ -16,6 +16,6 @@ import scipy
 # Are all values (other than the first column) floats (or able to be converted into floats)
 
 
-
+# Function 2: Preprocessing
 
 
