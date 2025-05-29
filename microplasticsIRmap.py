213399f737720117e85d229b098f73047ff7b06a
Michael Greenop <micgre@ceh.ac.uk>
2025-05-29 15:38:50 +0100

    Additional function outlines

--- microplasticsIRmap.py
+++ microplasticsIRmap.py
@@ -8,15 +8,18 @@ import seaborn
 import scipy
 
 
+# The aim of this script is to A) Format a dataset from Omnic data, but importantly
+# B) To provide a standardised formatting of ATR data from CSV files (naming, data types, etc.)
 
 
-# Function 1: Input data and format check (is the data in the format that the software requires?)
 
-# Is the first column that sample names?
-# Is the first column name "studyid"
-# Are the columns values (other than the first column) the wavenumber value (as a 2 decimal place float)
+# Function 1: Extracts, checks, and combines the spectra from CSV files into a formatted dataframe
+
+# Formatting check:
+# Have all the spectra in the files the same spectral range
+# Are the spectra named using a consistent naming style?
+# Is all the data the same data type (or can it be standardised?)
 # Are there any non-values?
-# Are all values (other than the first column) floats (or able to be converted into floats)
 
 
 # Function 2: Data seperation
