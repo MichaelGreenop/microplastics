f7a15e313d9bcb4896e1d5a3e911e435e2abefa1
Michael Greenop <micgre@ceh.ac.uk>
2025-05-29 10:27:50 +0100

    Start of the project

--- microplasticsIRmap.py
+++ microplasticsIRmap.py
@@ -7,7 +7,13 @@ import sklearn
 import seaborn
 import scipy
 
-print('all working now')
+# Function 1: Input data format check (is the data in the format that the software requires?)
+
+# Is the first column that sample names?
+# Is the first column name "studyid"
+# Are the columns values (other than the first column) the wavenumber value (as a 2 decimal place float)
+# Are there any non-values?
+# Are all values (other than the first column) floats (or able to be converted into floats)
 
 
 
